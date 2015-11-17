f5cefb09eca52f96051b69ae9b9f02c2cfba9848
kashamalasha <dmitry.burnyshev@gmail.com>
2015-11-18 00:01:18 +0300

    Marshaller and Unmarshaller work fine

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,7 @@
 import javax.xml.bind.JAXBContext;
 import javax.xml.bind.Marshaller;
+import javax.xml.bind.Unmarshaller;
+import java.io.File;
 import java.io.FileWriter;
 
 /**
@@ -7,23 +9,37 @@ import java.io.FileWriter;
  */
 public class Main {
     public static void main(String[] args) throws Exception{
-        Client client1 = new Client();
-        client1.setName("Dima");
-        client1.setPhone("89261580974");
+        /*Client client1 = new Client();
+        client1.setName("John");
+        client1.setPhone("+05522332344");
 
         Client client2 = new Client();
-        client2.setName("Katya");
-        client2.setPhone("89030014484");
+        client2.setName("Bill");
+        client2.setPhone("+05565666545");
 
         Clients clients = new Clients();
         clients.getClients().add(client1);
         clients.getClients().add(client2);
 
+        for (Client client : clients.getClients()) {
+            System.out.println(client.getName() + "\t" + client.getPhone());
+        }*/
+
+        /*
         JAXBContext context = JAXBContext.newInstance(Clients.class);
         Marshaller marshaller = context.createMarshaller();
 
         marshaller.setProperty(Marshaller.JAXB_FORMATTED_OUTPUT, true);
         marshaller.marshal(clients, new FileWriter("test.xml"));
+        */
+        File file = new File("test.xml");
+        JAXBContext context = JAXBContext.newInstance(Clients.class);
+        Unmarshaller unmarshaller = context.createUnmarshaller();
 
+        Clients clients = (Clients) unmarshaller.unmarshal(file);
+
+        for (Client client : clients.getClients()) {
+            System.out.println(client.getName() + "\t" + client.getPhone());
+        }
     }
 }
